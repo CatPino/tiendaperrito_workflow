10b9fe5e6b42267bb9af72e8e49f9d2ffd1e6141
CatPino <cat.pinos@duocuc.cl>
2026-05-18 23:46:48 +0000

    configuracion ip

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 //hola
 const {
-  DB_HOST = "10.0.2.134", 
+  DB_HOST = "10.0.159.89", 
   DB_USER = "root",
   DB_PASSWORD = "admin123",
   DB_NAME = "tienda_perritos",
